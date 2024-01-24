c697dab602eb4dc632402bf5bfdb8bfc358d3cb6
Youssef-Dahy <youssefdahy87@gmail.com>
2024-01-24 12:22:18 +0200

    initial commit

---
+++ assets/js/api.js
@@ -0,0 +1,25 @@
+"use strict";
+const api_key = "aed693500d1e8244988047f260f4a59f";
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
